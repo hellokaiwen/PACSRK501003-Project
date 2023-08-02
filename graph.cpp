9a636e5255417051e034c675a5b41bb44c0fbc02
Kaiwen Zheng <115329498+hellokaiwen@users.noreply.github.com>
2023-08-02 20:56:52 +0800

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,8 +1,17 @@
 #include <queue>
 #include <random>
 #include <iostream>
-#include "omp.h"
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <omp.h>
 #include "graph.h"
+
+using std::string;
+using std::ifstream;
+using std::stringstream;
+using std::vector;
+using std::unordered_map;
 
 VertexRefWMut Graph::AddVertex(vid_t vid) {
     auto vertex = Vertex(vid);
@@ -24,53 +33,62 @@ bool Graph::AddEdge(Vertex& v1, const Vertex& v2) {
     return v1.adjacent.insert(v2.vid).second;
 }
 
-Graph Graph::CreateRandom(size_t nodes, size_t edges) {
+/* Create a graph using data saved in file with the address *address*. */
+Graph Graph::Create(string address) {
     auto graph = Graph();
-    if (nodes == 0) return graph;
-    for (size_t i = 0; i < nodes; i++) {
-        graph.AddVertex(i);
+    ifstream file(address);
+    stringstream ss;
+    string data;
+    while (file) {
+        getline(file, data);
+        int del_pos = data.find(' ');
+        auto vid = std::stoi(data.substr(0, del_pos));
+        auto uid = std::stoi(data.substr(del_pos + 1, data.size() - del_pos - 1));
+        graph.AddVertex(vid);
+        graph.AddVertex(uid);
+        graph.AddEdge(graph.GetVertexMut(vid).value(), graph.GetVertexMut(uid).value());
     }
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, nodes - 1);
-    for (size_t i = 0; i < edges; i++) {
-        bool success;
-        do {
-            auto v1 = dis(gen);
-            auto v2 = dis(gen);
-            success = graph.AddEdge(graph.GetVertexMut(v1).value(), graph.GetVertex(v2).value());
-        } while (!success);
-    }
+    file.close();
     return graph;
 }
 
-void SerialBFS(const Graph& graph, const Vertex& start) {
-    std::queue<VertexRefW> queue;
-    std::unordered_set<vid_t> visited;
-    queue.emplace(start);
-    while (!queue.empty()) {
-        auto vertex = queue.front();
-        queue.pop();
-        if (visited.contains(vertex.get().vid)) continue;
-        // visit(vertex);
-        visited.insert(vertex.get().vid);
-        for (auto vid: vertex.get().adjacent) {
-            auto v = graph.GetVertex(vid);
-            if (v.has_value()) {
-                queue.push(v.value());
-            }
-        }
-    }
+const Vertex& Graph::RandomSrc() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, vertices_.size() - 1);
+    auto itr = vertices_.begin();
+    std::advance(itr, dis(gen));
+    return itr->second;
 }
 
-void Graph::BFS(const Vertex& start) {
-    std::unordered_map<vid_t, int> levels = {};
+//void SerialBFS(const Graph& graph, const Vertex& start) {
+//    std::queue<VertexRefW> queue;
+//    std::unordered_set<vid_t> visited;
+//    queue.emplace(start);
+//    while (!queue.empty()) {
+//        auto vertex = queue.front();
+//        queue.pop();
+//        if (visited.contains(vertex.get().vid)) continue;
+//        // visit(vertex);
+//        // std::cout << vertex.get().vid << " ";
+//        visited.insert(vertex.get().vid);
+//        for (auto vid: vertex.get().adjacent) {
+//            auto v = graph.GetVertex(vid);
+//            if (v.has_value()) {
+//                queue.push(v.value());
+//            }
+//        }
+//    }
+//}
+
+void Graph::SerialBFS(const Vertex& start) {
+    unordered_map<vid_t, int> levels = {};
     for (const auto& v : vertices_)
         levels.insert({v.first, -1});
     levels[start.vid] = 0;
     int level = 1;
-    std::vector<vid_t> frontier = {};
-    std::vector<vid_t> next_frontier = {};
+    vector<vid_t> frontier = {};
+    vector<vid_t> next_frontier = {};
     frontier.push_back(start.vid);
     // std::cout << start.vid << " ";
     while (!frontier.empty()) {
@@ -92,13 +110,13 @@ void Graph::BFS(const Vertex& start) {
 }
 
 void Graph::ParallelBFS(const Vertex& start, int num_threads) {
-    std::unordered_map<vid_t, int> levels = {};
+    unordered_map<vid_t, int> levels = {};
     for (const auto& v : vertices_)
         levels.insert({v.first, -1});
     levels[start.vid] = 0;
     int level = 1;
-    std::vector<vid_t> frontier = {};
-    std::vector<vid_t> next_frontier = {};
+    vector<vid_t> frontier = {};
+    vector<vid_t> next_frontier = {};
     frontier.push_back(start.vid);
     // std::cout << start.vid << " ";
     while (!frontier.empty()) {
